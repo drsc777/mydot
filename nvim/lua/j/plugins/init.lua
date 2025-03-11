94c01ceba15c83163f4e7d98595dc2ee1ffbb189
drsc777 <116252720+drsc777@users.noreply.github.com>
2025-03-10 20:06:41 -0700

    修复 Neovim 配置 | Fix Neovim configuration

---
+++ nvim/lua/j/plugins/init.lua
@@ -0,0 +1,15 @@
+-- 将所有插件文件导出为模块表
+return {
+  require("j.plugins.alpha"),
+  require("j.plugins.ale"),
+  require("j.plugins.autopairs"),
+  require("j.plugins.comment"),
+  require("j.plugins.gruvbox"),
+  require("j.plugins.lsp-zero"),
+  require("j.plugins.lualine"),
+  require("j.plugins.nvim-tree"),
+  require("j.plugins.nvim-treesitter"),
+  require("j.plugins.telescope"),
+  require("j.plugins.tmux-navigator"),
+  require("j.plugins.which-key"),
+} 
